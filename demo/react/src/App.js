398acdd8d7f3e7d4f475d27335999daafc6bab27
Miriam Müller <74644501+m1rm@users.noreply.github.com>
2026-05-02 13:23:38 +0200

    feat(#181): add onCountryClick callback including demo examples (#196)

--- demo/react/src/App.js
+++ demo/react/src/App.js
@@ -6,38 +6,58 @@ import './App.css';
 import svgMap from 'svgmap';
 import 'svgmap/style';
 
+const sampleGdpData = {
+  data: {
+    gdp: {
+      name: 'GDP per capita',
+      format: '{0} USD',
+      thousandSeparator: ',',
+      thresholdMax: 50000,
+      thresholdMin: 1000
+    },
+    change: {
+      name: 'Change to year before',
+      format: '{0} %'
+    }
+  },
+  applyData: 'gdp',
+  values: {
+    AF: { gdp: 587, change: 4.73 },
+    AL: { gdp: 4583, change: 11.09 },
+    DZ: { gdp: 4293, change: 10.01 }
+    // ...
+  }
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clickDemoSelection: 'Click a country on the map.'
+    };
+  }
 
   componentDidMount() {
     if (!this.svgMap) {
-
-      var mySvgMap = new svgMap({
+      this.svgMap = new svgMap({
         targetElementID: 'svgMap',
-        data: {
-          data: {
-            gdp: {
-              name: 'GDP per capita',
-              format: '{0} USD',
-              thousandSeparator: ',',
-              thresholdMax: 50000,
-              thresholdMin: 1000
-            },
-            change: {
-              name: 'Change to year before',
-              format: '{0} %'
-            }
-          },
-          applyData: 'gdp',
-          values: {
-            AF: { gdp: 587, change: 4.73 },
-            AL: { gdp: 4583, change: 11.09 },
-            DZ: { gdp: 4293, change: 10.01 }
-            // ...
-          }
+        data: sampleGdpData
+      });
+    }
+
+    if (!this.clickDemoMap) {
+      const clickDemoMap = new svgMap({
+        targetElementID: 'svgMapClickCallback',
+        data: sampleGdpData,
+        onCountryClick: (countryID) => {
+          const label = clickDemoMap.countries[countryID] || countryID;
+          this.setState({
+            clickDemoSelection: `Selected: ${label} (${countryID})`
+          });
+          return false;
         }
       });
-
-      this.svgMap = mySvgMap;
+      this.clickDemoMap = clickDemoMap;
     }
   }
 
@@ -45,7 +65,17 @@ class App extends Component {
     return (
       <div className='app'>
         <h1>svgMap React demo</h1>
-        <div id='svgMap'></div>
+
+        <section className='demo-section'>
+          <h2>GDP per capita (sample)</h2>
+          <div id='svgMap'></div>
+        </section>
+
+        <section className='demo-section'>
+          <h2>Country click callback</h2>
+          <div id='svgMapClickCallback'></div>
+          <div className='demo-click-result'>{this.state.clickDemoSelection}</div>
+        </section>
       </div>
     );
   }
